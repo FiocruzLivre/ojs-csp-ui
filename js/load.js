09d2681ad3626d7edb6425b8f3084f314eed6a0c
Lívia Gouvêa <livia@mail.mayfirst.org>
2026-03-04 13:33:51 -0300

    Remove customização de vue para upload de arquivos em submissão pois não estava sobrepondo ao do OJS

--- js/load.js
+++ js/load.js
@@ -1,9 +1,5 @@
 import SubmissionsListPanel from '@cspWorkflow/components/ListPanel/submissions/SubmissionsListPanel.vue';
 import SubmissionsListItem from '@cspWorkflow/components/ListPanel/submissions/SubmissionsListItem.vue';    
-import SubmissionFilesListPanel from '@cspWorkflow/components/ListPanel/submissionFiles/SubmissionFilesListPanel.vue';
-import SubmissionFilesListItem from '@cspWorkflow/components/ListPanel/submissionFiles/SubmissionFilesListItem.vue';
 // Add to controllers used by OJS
 window.pkp.controllers.Container.components.SubmissionsListPanel = SubmissionsListPanel;
 window.pkp.controllers.Container.components.SubmissionsListItem = SubmissionsListItem;
-window.pkp.controllers.Container.components.SubmissionFilesListPanel = SubmissionFilesListPanel;
-window.pkp.controllers.Container.components.SubmissionFilesListItem = SubmissionFilesListItem;
